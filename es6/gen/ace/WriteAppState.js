9e94592dbe2108a8a66adf1d950522febd5cda37
Annette Pohl <annette_pohl@web.de>
2019-02-11 19:59:30 +0100

    new generator

--- es6/gen/ace/WriteAppState.js
+++ es6/gen/ace/WriteAppState.js
@@ -1,3 +1,22 @@
+/* 
+ * Copyright (c) 2019, Annette Pohl, Koblenz, Germany
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+
+
+
 import AppUtils from "../../src/app/AppUtils";
 
 export let state;
@@ -13,11 +32,19 @@ export function set_state_State_filter(eventData) {
 export function reset_state_State_filter() {
 	state.filter = null;
 }
+
+export function init_state_State_filter() {
+	state.filter = null;
+}
 export function set_state_State_newTodo(eventData) {
 	state.newTodo = eventData.newTodo;
 }
 
 export function reset_state_State_newTodo() {
+	state.newTodo = null;
+}
+
+export function init_state_State_newTodo() {
 	state.newTodo = null;
 }
 export function set_state_State_loading(eventData) {
@@ -27,11 +54,19 @@ export function set_state_State_loading(eventData) {
 export function reset_state_State_loading() {
 	state.loading = null;
 }
+
+export function init_state_State_loading() {
+	state.loading = null;
+}
 export function set_state_State_todoList(eventData) {
 	state.todoList = eventData.todoList;
 }
 
 export function reset_state_State_todoList() {
+	state.todoList = null;
+}
+
+export function init_state_State_todoList() {
 	state.todoList = null;
 }
 export function set_state_State_editedTodo(eventData) {
@@ -54,11 +89,19 @@ export function reset_state_State_editedTodo() {
 	state.editedTodo = null;
 }
 
+export function init_state_State_editedTodo() {
+	state.editedTodo = {};
+}
+
 export function set_state_State_editedTodo_EditedTodo_editedDescription(eventData) {
 	state.editedTodo.editedDescription = eventData.editedDescription;
 }
 
 export function reset_state_State_editedTodo_EditedTodo_editedDescription() {
+	state.editedTodo.editedDescription = null;
+}
+
+export function init_state_State_editedTodo_EditedTodo_editedDescription() {
 	state.editedTodo.editedDescription = null;
 }
 export function set_state_State_editedTodo_EditedTodo_id(eventData) {
@@ -68,11 +111,19 @@ export function set_state_State_editedTodo_EditedTodo_id(eventData) {
 export function reset_state_State_editedTodo_EditedTodo_id() {
 	state.editedTodo.id = null;
 }
+
+export function init_state_State_editedTodo_EditedTodo_id() {
+	state.editedTodo.id = null;
+}
 export function set_state_State_editedTodo_EditedTodo_description(eventData) {
 	state.editedTodo.description = eventData.description;
 }
 
 export function reset_state_State_editedTodo_EditedTodo_description() {
+	state.editedTodo.description = null;
+}
+
+export function init_state_State_editedTodo_EditedTodo_description() {
 	state.editedTodo.description = null;
 }
 
---
+++ src-gen/todo/actions/AbstractInitAction.js
@@ -0,0 +1,17 @@
+import Action from "../../ace/SynchronousAction";
+import InitCommand from "../../../src/todo/commands/InitCommand";
+
+export default class AbstractInitAction extends Action {
+
+    constructor( hash) {
+        super({hash}, 'todo.InitAction');
+		}
+		
+	getCommand() {
+		return new InitCommand(this.actionData);
+	}
+
+
+}
+
+/*       S.D.G.       */
